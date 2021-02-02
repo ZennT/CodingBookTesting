bc07b2c55f1b1095ef62edf5dbdcb9eb811dd41d
ZennT <cinaralti80>
2021-02-01 22:42:44 -0800

    added classes inside pages package

---
+++ src/test/java/com/CodingBook/pages/MainPage.java
@@ -0,0 +1,12 @@
+package com.CodingBook.pages;
+
+import org.openqa.selenium.support.FindBy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainPage extends Base{
+    Logger logger = LoggerFactory.getLogger(MainPage.class);
+
+
+//    @FindBy(xpath = )
+}
