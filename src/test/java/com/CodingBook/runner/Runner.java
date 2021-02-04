3864581a10d5d0941e4d5b272a9a7a60cdac35e5
ZennT <cinaralti80>
2021-02-04 13:20:39 -0800

    completed runner class

--- src/test/java/com/CodingBook/runner/Runner.java
+++ src/test/java/com/CodingBook/runner/Runner.java
@@ -1,4 +1,20 @@
 package com.CodingBook.runner;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        // Glue path java folderdan sonra baslamali. src/test/java dan sonra
+        glue = "com/CodingBook/stepdef",
+        dryRun =false,
+        strict = true,
+//        tags = "@smoke_test,@unit_test",
+        plugin = {"html:target/default-cucumber-reports",
+                "json:target/cucumber.json"
+        }
+)
 public class Runner {
 }
